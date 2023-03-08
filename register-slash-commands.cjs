00e0787581633eb383a8d007735da2ae99bc3a16
just some guy <randy.bailey0@mycampus.apus.edu>
2023-03-08 13:51:27 -1000

    updating to use config.js

--- register-slash-commands.cjs
+++ register-slash-commands.cjs
@@ -4,7 +4,7 @@ const {
 const {
     Routes
 } = require('discord-api-types/v9');
-const config = require('./config.json');
+const config = require('./config.js');
 const commands = require('./commands.json');
 
 const rest = new REST({
