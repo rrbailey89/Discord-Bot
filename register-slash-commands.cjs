a9597293e0f742c6b5b542f84771404ca5e2acc1
just some guy <randy.bailey0@mycampus.apus.edu>
2023-03-08 14:18:07 -1000

    reformatted file and awaited

--- register-slash-commands.cjs
+++ register-slash-commands.cjs
@@ -1,29 +1,17 @@
-const {
-    REST
-} = require('@discordjs/rest');
-const {
-    Routes
-} = require('discord-api-types/v9');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
 let config;
+let commands;
 
 (async () => {
     config = await import('./config.js');
-    commands = await import('./commands.js')
-})();
+    commands = await import('./commands.js');
+    const rest = new REST({ version: '9' }).setToken(config.default.token);
 
-const rest = new REST({
-    version: '9'
-}).setToken(config.token);
-
-(async() => {
     try {
         console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationCommands(config.clientId), {
-            body: commands
-        }, );
-
+        await rest.put(Routes.applicationCommands(config.default.clientId), { body: commands.default });
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
