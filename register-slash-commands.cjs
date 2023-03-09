30df6af44ca810f638b2cc6679c5400df241cdb3
just some guy <randy.bailey0@mycampus.apus.edu>
2023-03-08 14:19:05 -1000

    removed defaults

--- register-slash-commands.cjs
+++ register-slash-commands.cjs
@@ -7,11 +7,11 @@ let commands;
 (async () => {
     config = await import('./config.js');
     commands = await import('./commands.js');
-    const rest = new REST({ version: '9' }).setToken(config.default.token);
+    const rest = new REST({ version: '9' }).setToken(config.token);
 
     try {
         console.log('Started refreshing application (/) commands.');
-        await rest.put(Routes.applicationCommands(config.default.clientId), { body: commands.default });
+        await rest.put(Routes.applicationCommands(config.clientId), { body: commands });
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
