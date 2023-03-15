4a0e39d49a0d667360e504b6ed319a20b05af5d2
just some guy <randy.bailey0@mycampus.apus.edu>
2023-03-15 09:46:27 -1000

    updating git cache

--- config.js
+++
@@ -1,7 +0,0 @@
-export default {
-	token: 'MTA3OTg1OTU0NDAyNjUyMTczMA.GXvgXI.7iCxkI2dm1kFAGPa_n0Lyye1FYpy_TdY87M4rY',
-	prefix: '',
-	cooldown: 300000,
-	clientId: '1079859544026521730',
-	xivApiKey: '0b6d637034bc4eef80ae8aef3e3a722b5eb0d9645aed444db50476838c5cabb2',
-};
