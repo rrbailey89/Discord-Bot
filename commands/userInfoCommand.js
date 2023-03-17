7e3e632412f8597674167dbcf17df2802a7e0786
whos there <randy.bailey0@mycampus.apus.edu>
2023-03-16 23:18:59 -1000

    remove the no longer needed commands.js

---
+++ commands/userInfoCommand.js
@@ -0,0 +1,39 @@
+import { UserContextMenuCommandInteraction, EmbedBuilder } from 'discord.js';
+
+export default {
+    name: 'User Information',
+    async execute(interaction) {
+        if (!interaction instanceof UserContextMenuCommandInteraction) 
+        return;
+    }
+
+    const member = interaction.targetMember;
+    const roles = member.roles.cache.filter(role => role.name !== '@everyone');
+    const rolesValue = roles.size ? roles.map(role => role.toString()).join(" ") : 'No Roles Assigned';
+    const embed = new EmbedBuilder()
+      .setTitle(`User Info - ${member.displayName}`)
+      .setImage(member.user.avatarURL({ dynamic: true, size: 4096 }))
+      .addFields({
+          name: 'Discord Name',
+          value: `${member.user.tag}`,
+          inline: true
+      }, {
+          name: 'Nickname',
+          value: `${member.displayName}`,
+          inline: true
+      }, {
+          name: 'Account Created On',
+          value: `${member.user.createdAt.toLocaleString()}`,
+          inline: true       
+       }, {
+          name: 'Joined Server On',
+          value: `${member.joinedAt.toLocaleString()}`,
+          inline: true
+       }, {
+          name: 'Roles',
+          value: `${rolesValue}`,
+          inline: true
+      } )
+      .setTimestamp();
+    await interaction.reply({ embeds: [embed] });
+    },
